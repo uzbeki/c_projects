0135734cff739f51ee2db24d56d470ccd5597579
Beki <uzbekitube@gmail.com>
2020-11-13 16:16:21 +0900

    cmake project created

---
+++ build/CMakeFiles/c_projects.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_projects.dir/main.cpp.obj"
+  "c_projects.exe"
+  "c_projects.exe.manifest"
+  "c_projects.pdb"
+  "libc_projects.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/c_projects.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
